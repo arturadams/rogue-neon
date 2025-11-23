5dbb09f416c03f4c33f3dc9ef9c015c51ab3020d
arturadams <artur_noia@yahoo.com.br>
2025-11-23 00:18:50 -0300

    Refactor game constants into modules

---
+++ configs/config.js
@@ -0,0 +1,22 @@
+export const CONFIG = {
+    spawnZ: -120,
+    playerZ: 40,
+    endZone: 50,
+    laneWidth: 24,
+    maxWaves: 20,
+    colors: {
+        cyan: 0x00ffff,
+        pink: 0xff00ff,
+        orange: 0xffaa00,
+        red: 0xff0055,
+        bg: 0x050510,
+        green: 0x00ff00,
+        yellow: 0xffee00,
+        common: 0xdddddd,
+        uncommon: 0x00ff00,
+        rare: 0x0088ff,
+        mythic: 0xaa00ff,
+        legendary: 0xffaa00,
+        cursed: 0xff0000,
+    },
+};
