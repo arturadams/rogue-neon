6b306931110ddd54bd594f9a1ecf128c803ea9ac
arturadams <artur_noia@yahoo.com.br>
2025-11-22 23:36:29 -0300

    Configure Vite server access

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  appType: "spa",
+  base: "./",
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: true,
+    port: 4173,
+    strictPort: true,
+  },
   build: {
     sourcemap: true,
   },
